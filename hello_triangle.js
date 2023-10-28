7b5a044b56450a3f4aacc9e6ebe88643f29e19d1
salsa-project <intenso524@gmail.com>
2023-10-28 07:46:40 +0200

    Hello Triangle

--- hello_triangle.js
+++ hello_triangle.js
@@ -9,6 +9,12 @@ function showError(errorText){
 showError('_______No ERROR :)_______')
 
 function helloTriangle(){
+
+    /* **************************** *
+    *  PHASE ONE: 
+    *           PREPARING DATA
+    * **************************** */
+
     const canvas = document.getElementById('canvas')
     if(!canvas){
         showError('Cannot get canvas reference - check for typos oe loading script too early in HTML')
@@ -19,9 +25,6 @@ function helloTriangle(){
         showError('This browser does not support WebGL2 - this demo will not work!')
         return;
     }
-    /* CLEAR CANVAS COLOR & DEPTH */
-    gl.clearColor(0.08, 0.08, 0.08, 1.0); // define the clear color
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); // clear color & depth
 
     const triangleVertices = [
         // Top middle
@@ -66,13 +69,12 @@ function helloTriangle(){
 
     // send the data to the gpu , BUT gpu can't read js variables
     // so we create a shader for the gpu
-    // first we createShader with type of (VERTEX_SHADER)
-    // then we shaderSource or we give it its data (vertexShaderSourceCode)
-    // then we compileShader
+    // first we 'createShader' with type of (VERTEX_SHADER)
+    // then we 'shaderSource' or we give it its data (vertexShaderSourceCode)
+    // then we 'compileShader'
     const vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderSourceCode)
     gl.compileShader(vertexShader)
-    
     // CHECK FOR ERROR IN THIS STEP ..
     if(!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)){
         const compileError = gl.getShaderInfoLog(vertexShader);
@@ -97,13 +99,12 @@ function helloTriangle(){
     `;
     // send the data to the gpu , BUT gpu can't read js variables
     // so we create a shader for the gpu
-    // first we createShader with type of (FRAGMENT_SHADER)
-    // then we shaderSource or we give it its data (fragmentShaderSourceCode)
-    // then we compileShader
+    // first we 'createShader' with type of (FRAGMENT_SHADER)
+    // then we 'shaderSource' or we give it its data (fragmentShaderSourceCode)
+    // then we 'compileShader'
     const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentShaderSourceCode)
     gl.compileShader(fragmentShader)
-    
     // CHECK FOR ERROR IN THIS STEP ..
     if(!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)){
         const compileError = gl.getShaderInfoLog(fragmentShader);
@@ -111,7 +112,67 @@ function helloTriangle(){
         return;
     }
 
+    // now using our vertexShader and fragmentShader we create a program
+    // we attach both vertexShader and fragmentShader with 'attachShader' to the program 
+    // then we link the program using 'linkProgram'
+    const triangleShaderProgram = gl.createProgram()
+    gl.attachShader(triangleShaderProgram, vertexShader)
+    gl.attachShader(triangleShaderProgram, fragmentShader)
+    gl.linkProgram(triangleShaderProgram)
+    // CHECK FOR ERROR IN THIS STEP ..
+    if(!gl.getProgramParameter(triangleShaderProgram, gl.LINK_STATUS)){
+        const compileError = gl.getShaderInfoLog(triangleShaderProgram);
+        showError(`Failde to LINK shaders - ${compileError}`);
+        return;
+    }
 
+    // using 'getAttribLocation' to get the 'vertexPosition'
+    const vertexPositionAttribLocation = gl.getAttribLocation(triangleShaderProgram, 'vertexPosition');
+    // CHECK FOR ERROR IN THIS STEP ..
+    if(vertexPositionAttribLocation < 0){
+        showError(`Failde to get attrib location!`);
+        return;
+    }
+
+
+    /* **************************** *
+    *  PHASE TWO: 
+    *           Pipeline..
+    * **************************** */
+
+    // Output merger - how to merge the shaded pixel fragment with the existing output image
+    canvas.height = canvas.clientHeight;
+    canvas.width = canvas.clientWidth;
+    gl.clearColor(0.08, 0.08, 0.08, 1.0); // define the clear color
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); // clear color & depth
+
+    // Rasterizer - wich pixels are part of triangle
+    gl.viewport(0, 0, canvas.width, canvas.height)
+
+    // Set GPU program (vertex + fragment shader pair)
+    gl.useProgram(triangleShaderProgram)
+    gl.enableVertexAttribArray(vertexPositionAttribLocation)
+
+    // Input assembler - how to read vertices from our gpu triangle buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleGeoBuffer);
+    gl.vertexAttribPointer(
+        /* index: which attribute to use */
+       vertexPositionAttribLocation,
+       /* size: how many components in that attribute*/
+       2,
+       /* type: what is the data type stored in the GPU buffer for this attribute? */
+       gl.FLOAT,
+       /* normalized: detemines how to convert ints to floats, is that's what you're doing */
+       false, 
+       /* stride: how many bytes to move forward in the buffer to find the same attribute for the next vertex */
+       /* 0: means the gpu will figure it out OR we can do it ourselves */
+       2 * Float32Array.BYTES_PER_ELEMENT , 
+       /* offset : how many bytes should the input assembler skip into the buffer when reading attributes */
+       0
+   )
+
+    // Draw call (also configures primitive assembly)
+    gl.drawArrays(gl.TRIANGLES, 0, 3)
 }
 
 try{
